7c10399b9bba4ae3585d786b0fec7117d838f104
vinodkumarpottupalli <pottupallivinod3@gmail.com>
2020-01-11 15:25:08 +0530

    git tuto

---
+++ git_example.py
@@ -0,0 +1,10 @@
+"""
+Creating alias for git log
+git config --global alias.hist "log --oneline --graph --decorate --all"
+git hist
+"""
+"""
+.gitignore for excluding unwanted files
+>> vi .gitignore
+>>*.log which excludes .log format file
+"""
